8af1cada76063a53c9429bbde5e5fd92f8ad1c93
Michael B Ansong <michael.ansong7@gmail.com>
2021-01-04 02:29:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,5 +16,4 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |maybe_call|
-    valid_calls.include?(maybe_call)
 end
