9f5bb9f3fb57f86300d667708ef4abdcf266411e
Michael B Ansong <michael.ansong7@gmail.com>
2021-01-04 02:22:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,11 +8,6 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |planeteer| planeteer.capitalize + "!" }
 end
 
-=begin def long_planeteer_calls(planeteer_calls)
-  # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
-  # Return the boolean result of this check
-end
-=end
 
 def long_planeteer_calls(planeteer_calls)
    planeteer_calls.any? { |word| word.length > 4}
