a859fbc5e8cab0d03af706f3a3c4adb04126b2f0
Michael B Ansong <michael.ansong7@gmail.com>
2021-01-04 02:28:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,6 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  cheesy_items.find do |maybe_cheese|
+  planeteer_calls.find do |maybe_cheese|
     cheeses_type.include?(maybe_cheese)
 end
